ffd58798de9f21938daf4fe53d1d12e3b9e3520c
Brian Savage <brianrsavage@users.noreply.github.com>
2019-03-30 15:56:33 -0400

    Update rwd-004-008-picking-breakpoints-2-exercise-assignment.js

--- rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.js
+++ rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.js
@@ -1,10 +1,9 @@
-    document.body.innerHTML = '<div id="dimensions"></div>' + document.body.innerHTML;
-
-    window.onresize = displayWindowSize;
-    window.onload = displayWindowSize;
-
-    function displayWindowSize() {
-        myWidth = window.innerWidth;
-        myHeight = window.innerHeight;
-        document.getElementById("dimensions").innerHTML = myWidth + "x" + myHeight;
-    }
+document.getElementsByTagName( "head" )[0].appendChild( '<link rel="stylesheet" type="text/css" href="https://reading-area-community-college.github.io/RWD/rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.css">' );
+document.body.innerHTML = '<div id="dimensions"></div>' + document.body.innerHTML;
+window.onresize = displayWindowSize;
+window.onload = displayWindowSize;
+function displayWindowSize() {
+    myWidth = window.innerWidth;
+    myHeight = window.innerHeight;
+    document.getElementById("dimensions").innerHTML = myWidth + "x" + myHeight;
+}
