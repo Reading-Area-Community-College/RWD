cca4771c12f75fa13091e5139dbd13b9aab49214
Brian Savage <brianrsavage@users.noreply.github.com>
2019-03-30 16:30:53 -0400

    Update rwd-004-008-picking-breakpoints-2-exercise-assignment.js

--- rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.js
+++ rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.js
@@ -1,9 +1,17 @@
-
-var element = document.createElement("https://reading-area-community-college.github.io/RWD/rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.css");
+var basePath = '"https://reading-area-community-college.github.io/RWD/rwd-004-008-picking-breakpoints-2-exercise-assignment/';
+var element = document.createElement(basePath+'rwd-004-008-picking-breakpoints-2-exercise-assignment.css");
 element.setAttribute("rel", "stylesheet");
 element.setAttribute("type", "text/css");
 element.setAttribute("href", "external.css");
 document.getElementsByTagName("head")[0].appendChild(element);
+
+
+var imgSources = Array.from(xmlDoc.getElementsByTagName("img"));
+var i = 0;
+do {
+    document.getElementsByTagName(cityNames[i]).src = (basePath+'images/'+cityNames[i].src);
+    i++;
+}while (i < imgSources.length);
 
 document.body.innerHTML = '<div id="dimensions"></div>' + document.body.innerHTML;
 window.onresize = displayWindowSize;
