2696b2ccb06a28b69b60f7f5fbe89dd897d5970e
Brian Savage <brianrsavage@users.noreply.github.com>
2019-03-30 17:04:45 -0400

    Update rwd-004-008-picking-breakpoints-2-exercise-assignment.js

--- rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.js
+++ rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.js
@@ -4,16 +4,12 @@ element.setAttribute("rel", "stylesheet");
 element.setAttribute("type", "text/css");
 element.setAttribute("href", "external.css");
 document.getElementsByTagName("head")[0].appendChild(element);
-
-
-var imgSources = Array.from(document.getElementsByTagName("img"));
 var i = 0;
 do {
-    document.body.innerHTML = '<di>'+imgSources[i].src+'</div>' + document.body.innerHTML;
-    document.getElementsByTagName(imgSources[i]).src = (basePath+'images/'+imgSources[i].src);
+    let newSrc = document.getElementsByTagName("img")[i].currentSrc.split('/').pop();
+    document.getElementsByTagName("img")[i].src = basePath+'images/'+newSrc;
     i++;
-}while (i < imgSources.length);
-
+}while (i < document.getElementsByTagName("img").length);
 document.body.innerHTML = '<div id="dimensions"></div>' + document.body.innerHTML;
 window.onresize = displayWindowSize;
 window.onload = displayWindowSize;
