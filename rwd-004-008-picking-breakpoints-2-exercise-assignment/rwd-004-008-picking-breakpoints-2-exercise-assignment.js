4fe7d9b87ab1c79aaef289e23903c44f88488f5f
Brian Savage <brianrsavage@users.noreply.github.com>
2019-03-30 17:08:26 -0400

    Update rwd-004-008-picking-breakpoints-2-exercise-assignment.js

--- rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.js
+++ rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.js
@@ -1,18 +1,18 @@
 var basePath = 'https://reading-area-community-college.github.io/RWD/rwd-004-008-picking-breakpoints-2-exercise-assignment/';
-var element = document.createElement(basePath+'rwd-004-008-picking-breakpoints-2-exercise-assignment.css');
-element.setAttribute("rel", "stylesheet");
-element.setAttribute("type", "text/css");
-element.setAttribute("href", "external.css");
-document.getElementsByTagName("head")[0].appendChild(element);
+
 var i = 0;
 do {
     let newSrc = document.getElementsByTagName("img")[i].currentSrc.split('/').pop();
     document.getElementsByTagName("img")[i].src = basePath+'images/'+newSrc;
     i++;
-}while (i < document.getElementsByTagName("img").length);
+}
+while (i < document.getElementsByTagName("img").length);
+
 document.body.innerHTML = '<div id="dimensions"></div>' + document.body.innerHTML;
+
 window.onresize = displayWindowSize;
 window.onload = displayWindowSize;
+
 function displayWindowSize() {
     myWidth = window.innerWidth;
     myHeight = window.innerHeight;
