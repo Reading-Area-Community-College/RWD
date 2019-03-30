ec236b63c226668e1e68fc75cf37e4d70d61c88a
Brian Savage <brianrsavage@users.noreply.github.com>
2019-03-30 17:11:46 -0400

    Update rwd-004-008-picking-breakpoints-2-exercise-assignment.js

--- rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.js
+++ rwd-004-008-picking-breakpoints-2-exercise-assignment/rwd-004-008-picking-breakpoints-2-exercise-assignment.js
@@ -1,12 +1,4 @@
-var basePath = 'https://reading-area-community-college.github.io/RWD/rwd-004-008-picking-breakpoints-2-exercise-assignment/';
 
-var i = 0;
-do {
-    let newSrc = document.getElementsByTagName("img")[i].currentSrc.split('/').pop();
-    document.getElementsByTagName("img")[i].src = basePath+'images/'+newSrc;
-    i++;
-}
-while (i < document.getElementsByTagName("img").length);
 
 document.body.innerHTML = '<div id="dimensions"></div>' + document.body.innerHTML;
 
