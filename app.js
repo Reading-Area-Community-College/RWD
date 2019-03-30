3f384cd7cc4a6dcd47df2968e65a9edf82a945cb
Brian Savage <brianrsavage@users.noreply.github.com>
2019-03-30 15:36:13 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+    document.body.innerHTML = '<div id="dimensions"></div>' + document.body.innerHTML;
+
+    window.onresize = displayWindowSize;
+    window.onload = displayWindowSize;
+
+    function displayWindowSize() {
+        myWidth = window.innerWidth;
+        myHeight = window.innerHeight;
+        document.getElementById("dimensions").innerHTML = myWidth + "x" + myHeight;
+    }
